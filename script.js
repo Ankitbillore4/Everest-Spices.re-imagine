61ad1dedd9cec488ea6dbdbdca22f58501a8009c
billoreankit66@gmail.com <billoreankit66@gmail.com>
2024-07-16 00:24:08 +0530

    new commit

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
 var tl= gsap.timeline({scrollTrigger:{
-    trigger:".main",
+    trigger:".footer",
     start:"55% 50%",
     end:"100% 50%",
     scrub:2,
     pin:true,
-    markers:true
+
 }})
 
 tl.to(".top",{
@@ -13,17 +13,17 @@ tl.to(".top",{
 },"a")
 
 tl.to(".bottom",{
-    bottom:"-50%"
+    bottom:"-50%",
 },"a")
 
 tl.to(".top-h1",{
-    top:"70%",
+    top:"100%",
 },"a")
 
 tl.to(".bottom-h1",{
     top:"-80%",
 },"a")
 
-tl.to(".contant ",{
+tl.to(".content ",{
     marginTop:"0%",
 },"a")
