7779f15f8d8bcd58a29c496b18c6a41ffa335398
billoreankit66@gmail.com <billoreankit66@gmail.com>
2024-07-18 15:27:54 +0530

    add

--- script.js
+++ script.js
@@ -54,40 +54,44 @@ elems.forEach(function(e){
 
 
 
-// const scroll = new LocomotiveScroll({
-//     el: document.querySelector('.mainparent'),
-//     smooth: true
-// });
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('.mainparent'),
+    smooth: true
+});
 
 
-// gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger);
 
-// // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
 
-// const locoScroll = new LocomotiveScroll({
-//   el: document.querySelector(".mainparent"),
-//   smooth: true
-// });
-// // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-// locoScroll.on("scroll", ScrollTrigger.update);
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector(".mainparent"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
 
-// // tell ScrollTrigger to use these proxy methods for the ".mainparent" element since Locomotive Scroll is hijacking things
-// ScrollTrigger.scrollerProxy(".mainparent", {
-//   scrollTop(value) {
-//     return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-//   }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-//   getBoundingClientRect() {
-//     return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-//   },
-//   // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-//   pinType: document.querySelector(".mainparent").style.transform ? "transform" : "fixed"
-// });
+// tell ScrollTrigger to use these proxy methods for the ".mainparent" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy(".mainparent", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector(".mainparent").style.transform ? "transform" : "fixed"
+});
 
-// // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
-// ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
-// // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-// ScrollTrigger.refresh();
+
+
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
 
 
 
@@ -98,12 +102,13 @@ elems.forEach(function(e){
 // MASALA SCROLLING...
 
 gsap.to(".tandurimasalaimage",{
+    scale:"0.9",
     scrollTrigger:{
         trigger:".tandurimasalaimage",
-        scroller:"body",
+        scroller:".mainparent",
         markers:true,
-        start:"top 12%",
-        end:"top -320%",
+        start:"top 15%",
+        end:"top -319%",
         scrub:2,
         pin:true,
     }
@@ -112,9 +117,10 @@ gsap.to(".tandurimasalaimage",{
 
 
 gsap.to(".tandurimasalaimage",{
+    
     scrollTrigger:{
         trigger:".page5 h1",
-        scroller:"body",
+        scroller:".mainparent",
         markers:true,
         start:"top 316%",
         end:"top -316%",
